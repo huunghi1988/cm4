c87ba26396583b380a2d994fca2b77f17d11c98a
nghitran <nghitran@localhost>
2023-05-02 22:42:10 +1000

    short Array

---
+++ cm4big0/src/cm4big0/ShortArray.java
@@ -0,0 +1,33 @@
+package cm4big0;
+
+import java.util.Arrays;
+
+public class ShortArray {
+	public static void main(String[] args) {
+
+		// giving an array containing 0,1,2 only. write a function to short array in
+		// O(n) time
+		int[] array = { 1, 0, 0, 0, 0, 1, 2, 2, 1 };
+		int[] shortArray = new int[array.length];
+		Arrays.fill(shortArray, 0);
+		int countOne = 0;
+		int countTwo = 0;
+		for (int i = 0; i < array.length; i++) {
+			if (array[i] == 1) {
+				countOne = countOne + 1;
+			}
+
+			else if (array[i] == 2) {
+				countTwo = countTwo + 1;
+			}
+		}
+
+		Arrays.fill(shortArray, array.length - countTwo, array.length, 2);
+		Arrays.fill(shortArray, array.length - countTwo - countOne, array.length - countTwo, 1);
+		System.out.print("Sorted array : ");
+		for (int x = 0; x < shortArray.length; x++)
+			System.out.print(shortArray[x] + " ");
+		System.out.println();
+		System.out.println("O(2n)=>O(n)");
+	}
+}
