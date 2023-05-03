1f79632275a51df83bc85bcb0ecf4720d15dd5d4
nghitran <nghitran@localhost>
2023-05-03 20:10:39 +1000

    update

--- cm4big0/src/cm4big0/LongestPrefix.java
+++ cm4big0/src/cm4big0/LongestPrefix.java
@@ -1,12 +1,19 @@
 package cm4big0;
 
+import java.util.Arrays;
+
 public class LongestPrefix {
 	public static void main(String[] args) {
-		// String[] strs = { "flower", "flow", "floight", "flo" };
-		String[] strs = { "flll","fl","fa" };
+		String[] strs3 = { "flower", "flow", "floight", "flo" };
+		String[] strs = { "faa", "fbbbb", "fab" };
+		String[] strs2 = { "flll", "fl", "fa" };
 
 		longestCommonPrefix(strs);
 
+		longestCommonPrefix(strs2);
+		longestCommonPrefix(strs3);
+
+		System.out.println("BigO is O(n)");
 	}
 
 	public static void longestCommonPrefix(String[] strs) {
@@ -20,7 +27,7 @@ public class LongestPrefix {
 			System.out.println("no prefix ");
 			return;
 		}
-		//array longer than 1
+		// array longer than 1
 		if (strs.length > 1) {
 			// get shortest string length
 			int shortestString = strs[0].length();
@@ -30,17 +37,19 @@ public class LongestPrefix {
 					shortestString = strs[i].length();
 			}
 
+			System.out.println(shortestString);
 			int index = 0;
-			for (int i = 0; (i < shortestString && strs[0].charAt(i) == strs[strs.length - 1].charAt(i)); i++) {
 
-				{
-					index = i + 1;
-				}
+			Arrays.sort(strs);
+			while (index < shortestString && strs[0].charAt(index) == strs[strs.length - 1].charAt(index)) {
+				index++;
 
 			}
+
+			System.out.println(Arrays.toString(strs));
 			System.out.println("longest prefix is : " + strs[0].substring(0, index));
 			// 2 loop => O(2n)=>O(n)
-			System.out.println("BigO is O(n)");
+
 		}
 	}
 
