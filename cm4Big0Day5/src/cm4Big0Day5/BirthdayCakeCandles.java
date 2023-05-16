1b8457843268d8d6cf76ab2c6e4e83ae1ce97d29
nghitran <nghitran@localhost>
2023-05-16 20:57:42 +1000

    new

---
+++ cm4Big0Day5/src/cm4Big0Day5/BirthdayCakeCandles.java
@@ -0,0 +1,18 @@
+package cm4Big0Day5;
+
+import java.util.List;
+
+public class BirthdayCakeCandles {
+public static void main(String[] args) {
+	
+}
+public static int birthdayCakeCandles(List <Integer> candles) {
+	int count=0;
+	for(int i=1;i<candles.size();i++ )
+			{
+		if(candles[i]>candles[i-1])
+		{
+			}
+	return count;
+}
+}
