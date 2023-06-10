f5bb74d9ead6920a2d43930e0e79cc8b21aae29d
nghitran <nghitran@localhost>
2023-06-10 20:06:35 +1000

    edit

--- cinema/src/sqlday5.sql
+++ cinema/src/sqlday5.sql
@@ -2,11 +2,11 @@ CREATE DEFINER=`root`@`localhost` PROCEDURE `findMostBookedRoom`()
 BEGIN
 select room.name,room.id,count(distinct booking.customer_id)as cus
 from room join screening on room.id = screening.room_id
-join booking on film_id=booking.id
+join booking on screening.id=booking.screening_id
 group by room.name,room.id
 having cus= (select count(distinct booking.customer_id)as cus
 from room join screening on room.id = screening.room_id
-join booking on film_id=booking.id
+join booking on screening.id=booking.screening_id 
 group by room.name,room.id
 order by cus desc
 limit 1);
