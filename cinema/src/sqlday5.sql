a90bd07a9bf4950a9732ff4f603a884290df6ba7
nghitran <nghitran@localhost>
2023-06-10 20:11:25 +1000

    edit

--- cinema/src/sqlday5.sql
+++ cinema/src/sqlday5.sql
@@ -1,6 +1,6 @@
-CREATE DEFINER=`root`@`localhost` PROCEDURE `findMostBookedRoom`()
+CREATE DEFINER=`root`@`localhost` PROCEDURE `findMostBookedRoom`(out room_id int)
 BEGIN
-select room.name,room.id,count(distinct booking.customer_id)as cus
+with temptable as(select room.name,room.id,count(distinct booking.customer_id)as cus
 from room join screening on room.id = screening.room_id
 join booking on screening.id=booking.screening_id
 group by room.name,room.id
@@ -9,5 +9,9 @@ from room join screening on room.id = screening.room_id
 join booking on screening.id=booking.screening_id 
 group by room.name,room.id
 order by cus desc
-limit 1);
+limit 1))
+
+
+select id into room_id
+from temptable;
 END
