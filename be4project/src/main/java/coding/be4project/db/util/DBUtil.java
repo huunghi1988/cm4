bfb935bd841df69e9f192bd87696a18df0d25e61
Nghi Tran <huunghi1988@gmail.com>
2023-07-01 12:58:13 +1000

    new

--- be4project/src/main/java/coding/be4project/db/util/DBUtil.java
+++ be4project/src/main/java/coding/be4project/db/util/DBUtil.java
@@ -3,13 +3,15 @@ package coding.be4project.db.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.SQLOutput;
 
 public class DBUtil {
 	public static  Connection makeConnection() {
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection conn = DriverManager.getConnection("jdbc:mysql://50.87.144.214:3306/huunghit_cm4?useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true", "huunghit_root",
+			Connection conn = DriverManager.getConnection("jdbc:mysql://34.173.52.85/be4_project?useLegacyDatetimeCode=false&serverTimezone=UTC&useSSL=false&allowPublicKeyRetrieval=true", "huunghi_be4",
 					"P@ssword123");
+			System.out.println(conn);
 return conn;
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
